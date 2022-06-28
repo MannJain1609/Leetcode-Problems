8ec0f433f792f73813344a4d26bee7b9278f9025
Mann Jain <75989468+MannJain1609@users.noreply.github.com>
2022-06-28 21:25:45 +0530

    Time: 3 ms (76.84%), Space: 6 MB (73.40%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        //Using List
+        
+        // list<int> circle;
+        // for(int i=1; i<=n; i++)
+        //     circle.push_back(i);
+        // auto it = circle.begin();
+        // while(circle.size()>1)
+        // {
+        //     for(int i=1; i<k; i++)
+        //     {
+        //         it++;
+        //         if(it == circle.end())
+        //             it = circle.begin();
+        //     }
+        //     circle.erase(it++);
+        //     if(it == circle.end())
+        //         it = circle.begin();
+        // }
+        // return circle.back();
+        
+        //Josephus Problem
+        if (n==1)
+            return 1;
+        
+        return 1+(findTheWinner(n-1,k)+k-1)%n;
+    }
+};
