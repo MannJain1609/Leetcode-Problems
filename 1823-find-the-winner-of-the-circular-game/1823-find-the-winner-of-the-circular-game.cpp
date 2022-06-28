c6b4acaa9a85f62371fa22ce8a61fd11b34e7669
Mann Jain <75989468+MannJain1609@users.noreply.github.com>
2022-06-28 21:19:40 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (73.40%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        list<int> circle;
-        for(int i=1; i<=n; i++)
-            circle.push_back(i);
-        auto it = circle.begin();
-        while(circle.size()>1)
-        {
-            for(int i=1; i<k; i++)
-            {
-                it++;
-                if(it == circle.end())
-                    it = circle.begin();
-            }
-            circle.erase(it++);
-            if(it == circle.end())
-                it = circle.begin();
-        }
-        return circle.back();
+        // list<int> circle;
+        // for(int i=1; i<=n; i++)
+        //     circle.push_back(i);
+        // auto it = circle.begin();
+        // while(circle.size()>1)
+        // {
+        //     for(int i=1; i<k; i++)
+        //     {
+        //         it++;
+        //         if(it == circle.end())
+        //             it = circle.begin();
+        //     }
+        //     circle.erase(it++);
+        //     if(it == circle.end())
+        //         it = circle.begin();
+        // }
+        // return circle.back();
+        if (n==1)
+            return 1;
+        
+        return 1+(findTheWinner(n-1,k)+k-1)%n;
     }
 };
