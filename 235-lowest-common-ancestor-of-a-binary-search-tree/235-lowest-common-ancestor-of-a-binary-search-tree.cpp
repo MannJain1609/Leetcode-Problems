da730fe84bcfeff301ebf6f4bd4cd45ddb65ac8a
Mann Jain <75989468+MannJain1609@users.noreply.github.com>
2022-06-30 18:06:41 +0530

    Time: 36 ms (77.07%), Space: 23.2 MB (63.91%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,20 +11,30 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        int p_val = p->val;
-        int q_val = q->val;
-        int max_value = max(p_val,q_val);
-        int min_value = min(p_val,q_val);
-        TreeNode* temp = root;
-        while(temp)
-        {
-            if(temp == p || temp == q || (temp->val > min_value && temp->val < max_value))
-                return temp;
-            if(temp->val<min_value)
-                temp = temp->right;
-            else
-                temp = temp->left;
-        }
-        return temp;
+        // int p_val = p->val;
+        // int q_val = q->val;
+        // int max_value = max(p_val,q_val);
+        // int min_value = min(p_val,q_val);
+        // TreeNode* temp = root;
+        // while(temp)
+        // {
+        //     if(temp == p || temp == q || (temp->val > min_value && temp->val < max_value))
+        //         return temp;
+        //     if(temp->val<min_value)
+        //         temp = temp->right;
+        //     else
+        //         temp = temp->left;
+        // }
+        // return temp;
+        
+        //Recursion
+        //if both are smaller than root, go to left
+        if(root->val > p->val && root->val > q->val)
+            return lowestCommonAncestor(root->left,p,q);
+        //if both are greater than root
+        if(root->val < p->val && root->val < q->val)
+            return lowestCommonAncestor(root->right,p,q);
+        //else root is the LCA
+        return root;
     }
 };
