180236598bd54f07d2f22d10df7d13d3f1707f0e
Mann Jain <75989468+MannJain1609@users.noreply.github.com>
2022-07-02 20:22:19 +0530

    Time: 221 ms (70.59%), Space: 107.9 MB (39.65%) - LeetHub

---
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
@@ -0,0 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+class IndexedNode{
+public:
+    TreeNode* root;
+    int Min,Max;
+    bool BST;
+    IndexedNode(TreeNode* root, int Min, int Max, bool BST)
+    {
+        this->root = root;
+        this->Min = Min;
+        this->Max = Max;
+        this->BST = BST;
+    }
+};
+    //empty BST, ans will never be negative
+    int max_sum = 0;
+    IndexedNode MaxSumBST(TreeNode* root)
+    {
+        if(root == NULL)
+            return {root,INT_MIN,INT_MAX,true};
+        if(root->left == NULL && root->right == NULL) {
+            max_sum = max(max_sum,root->val);
+            return {root,root->val,root->val,true};
+        }
+        IndexedNode Left = MaxSumBST(root->left);
+        IndexedNode Right = MaxSumBST(root->right);
+        int minimum = root->val, maximum = root->val;
+        bool isBST = true;
+        if(Left.root) {
+            minimum = min(minimum,Left.Min);
+            if(!Left.BST || root->val <= Left.Max)
+                isBST = false;
+        }
+        if(Right.root) {
+            maximum = max(maximum,Right.Max);
+            if(!Right.BST || root->val >= Right.Min)
+                isBST = false;
+        }
+        IndexedNode temp = {root,minimum,maximum,isBST};
+        if(!isBST)
+            return temp;
+        if(root->left)
+            root->val += root->left->val;
+        if(root->right)
+            root->val+= root->right->val;
+        max_sum = max(max_sum,root->val);
+        return temp;
+    }
+public:
+    int maxSumBST(TreeNode* root) {
+        MaxSumBST(root);
+        return max_sum;
+    }
+};
