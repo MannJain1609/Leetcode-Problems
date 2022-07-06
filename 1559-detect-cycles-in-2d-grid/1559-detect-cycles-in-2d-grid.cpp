724b17f3eccc1cffc9689857b95714557c312bb3
Mann Jain <75989468+MannJain1609@users.noreply.github.com>
2022-07-06 14:06:30 +0530

    Time: 299 ms (85.99%), Space: 60.6 MB (56.93%) - LeetHub

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.cpp
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.cpp
@@ -1,20 +1,5 @@
 class Solution {
 private:
-//     struct hash_pair {
-//     template <class T1, class T2>
-//     size_t operator()(const pair<T1, T2>& p) const
-//     {
-//         auto hash1 = hash<T1>{}(p.first);
-//         auto hash2 = hash<T2>{}(p.second);
- 
-//         if (hash1 != hash2) {
-//             return hash1 ^ hash2;             
-//         }
-         
-//         // If hash1 == hash2, their XOR is zero.
-//           return hash1;
-//     }
-// };
     bool checkCycle(vector<vector<char>>& grid, vector<vector<bool>>& visited, 
                     int m, int n, int row, int col, int parent_row, int parent_col) 
     {
