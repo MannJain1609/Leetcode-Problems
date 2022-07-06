57b7972dbee230bfe1e1fb47fa95144438679c16
Mann Jain <75989468+MannJain1609@users.noreply.github.com>
2022-07-06 09:52:51 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (80.05%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -5,6 +5,13 @@ public:
             return 0;
         if(n == 1)
             return 1;
-        return fib(n-1) + fib(n-2);
+        int prev1 = 0,prev2 = 1,ans=1;
+        for(int i=2; i<=n; i++)
+        {
+            ans = prev1+prev2;
+            prev1 = prev2;
+            prev2 = ans;
+        }
+        return ans;
     }
 };
