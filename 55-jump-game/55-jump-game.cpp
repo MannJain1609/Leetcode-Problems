a383eaa97e7eb04f428f41316f42a3c7ef6b0a42
Mann Jain <75989468+MannJain1609@users.noreply.github.com>
2022-07-09 11:25:25 +0530

    Time: 71 ms (90.26%), Space: 48.2 MB (93.25%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int size = nums.size();
-        vector<bool>dp(size,false);
-        dp[size-1] = true;
-        for(int i=size-2; i>=0; i--)
-        {
-            int jump = nums[i];
-            if(jump+i>=size-1)
-                dp[i] = true;
-            else
-            {
-                for(int j= jump; j>0; j--)
-                {
-                    if(!dp[i])
-                        dp[i] = dp[i+j];
-                    else 
-                        break;
-                }
-            }
+       int farthest=0;
+        
+        for(int i=0;i<nums.size()-1;i++){
+            if(i+nums[i] > farthest)
+                farthest = i+nums[i];
+            if(i==farthest)
+                break;
         }
-        return dp[0];
+               
+        return farthest >= nums.size()-1;
     }
 };
