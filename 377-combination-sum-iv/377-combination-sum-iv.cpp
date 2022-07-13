33b09b8261b8e099dc8ebbb28af0f3eabdc7af5a
Mann Jain <75989468+MannJain1609@users.noreply.github.com>
2022-07-13 23:09:50 +0530

    Time: 3 ms (63.25%), Space: 6.6 MB (92.25%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,25 +1,27 @@
 class Solution {
-private:
-    int helper(vector<int>& nums, int target, int n,vector<int>& memo) {
-        if(target<0)
-            return 0;
-        if(target == 0) {
-            return 1;
-        }
-        if(memo[target]!=-1)
-            return memo[target];
-        int count = 0;
-        for(int i=0; i<n; i++)
-        {
-            count+= helper(nums,target-nums[i],n,memo);
-        }
-        return memo[target]= count;
-    }
+// private:
+//     int helper(vector<int>& nums, int target, int n,vector<int>& memo) {
+//         if(target<0)
+//             return 0;
+//         if(target == 0) {
+//             return 1;
+//         }
+//         if(memo[target]!=-1)
+//             return memo[target];
+//         int count = 0;
+//         for(int i=0; i<n; i++)
+//         {
+//             count+= helper(nums,target-nums[i],n,memo);
+//         }
+//         return memo[target]= count;
+//     }
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        int n = nums.size();
-        vector<int>memo(target+1,-1);
-        memo[0] = 1;
-        return helper(nums,target,n,memo);
+        vector<unsigned int> dp(target+1, 0);
+        dp[0] = 1;
+        for(int i=1; i<=target; ++i)
+            for(int j=0; j<nums.size(); ++j)
+                if(nums[j] <= i) dp[i] += dp[i-nums[j]];
+        return dp[target];
     }
 };
