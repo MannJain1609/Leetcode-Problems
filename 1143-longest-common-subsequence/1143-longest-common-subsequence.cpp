a4fe8c0a53ef15b466a7cee734485c78c7a11c7c
Mann Jain <75989468+MannJain1609@users.noreply.github.com>
2022-07-14 11:28:18 +0530

    Time: 27 ms (66.96%), Space: 13 MB (41.14%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,58 +1,19 @@
 class Solution {
-    int helper(string& text1, string& text2, int ind1, int ind2, int n, int m, vector<vector<int>>& memo) {
-        if(ind1>=n || ind2>=m)
-            return 0;
-        if(memo[ind1][ind2] !=-1)
-            return memo[ind1][ind2];
-        if(text1[ind1] == text2[ind2])
-            return memo[ind1][ind2] = 1 + helper(text1,text2,ind1+1,ind2+1,n,m,memo);
-        return memo[ind1][ind2] = 
-            max(helper(text1,text2,ind1,ind2+1,n,m,memo),helper(text1,text2,ind1+1,ind2,n,m,memo));
-    }
 public:
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.size();
         int m = text2.size();
-        vector<vector<int>>memo(n,vector<int>(m,-1));
-        return helper(text1,text2,0,0,n,m,memo);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=m; j++)
+            {
+                if(text1[i-1] == text2[j-1])
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[n][m];
     }
-   
-    
-//     int helper(string& str1, string& str2, int i1, int i2, int n1, int n2,vector<vector<int>>& dp)
-//     {
-//         // base case, if one of the string is empty
-        
-//         if(i1 == n1 || i2 == n2)
-//             return 0;
-        
-//         // if already calculated
-        
-//         if(dp[i1][i2] != -1)
-//             return dp[i1][i2];
-        
-//         // if characters are matching
-        
-//         if(str1[i1] == str2[i2])  
-//             return dp[i1][i2] = 1 + helper(str1, str2, i1 + 1, i2 + 1, n1, n2,dp);
-        
-//         // if characters are not matching
-        
-//         else
-//         {
-//             return dp[i1][i2] = max(helper(str1, str2, i1 + 1, i2, n1, n2,dp), helper(str1, str2, i1, i2 + 1, n1, n2,dp));
-//         }
-//     }
-    
-//     int longestCommonSubsequence(string str1, string str2) {
-        
-//         int n1 = str1.size();
-        
-//         int n2 = str2.size();
-		
-// 		// initialize dp
-        
-//         vector<vector<int>>dp(n1,vector<int>(n2,-1));
-        
-//         return helper(str1, str2, 0, 0, n1, n2,dp);
-//     }
 };
