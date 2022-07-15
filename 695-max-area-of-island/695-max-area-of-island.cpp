4b1370b8d4cf248cc06630a0cbd234f42e12d7cb
Mann Jain <75989468+MannJain1609@users.noreply.github.com>
2022-07-15 10:20:46 +0530

    Time: 35 ms (38.59%), Space: 23.9 MB (34.64%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,30 +1,29 @@
 class Solution {
 private:
-    int maxArea = 0,count = 0;
-    void dfs(vector<vector<int>>& grid,vector<vector<bool>>& visited, int m, int n, int i, int j)
+    int dfs(vector<vector<int>>& grid,vector<vector<bool>>& visited, int m, int n, int i, int j)
     {
         if(i<0 || j<0 || i>=m || j>=n || visited[i][j] || grid[i][j]==0)
-            return;
+            return 0;
         visited[i][j] = true;
-        count++;
-        dfs(grid,visited,m,n,i-1,j);
-        dfs(grid,visited,m,n,i+1,j);
-        dfs(grid,visited,m,n,i,j-1);
-        dfs(grid,visited,m,n,i,j+1);
-        maxArea = max(maxArea,count);
+        
+        int u = dfs(grid,visited,m,n,i-1,j);
+        int d = dfs(grid,visited,m,n,i+1,j);
+        int l = dfs(grid,visited,m,n,i,j-1);
+        int r = dfs(grid,visited,m,n,i,j+1);
+        return(u+d+l+r+1);
     }
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
         vector<vector<bool>>visited(m,vector<bool>(n,false));
+        int maxArea = 0;
         for(int i=0; i<m; i++)
         {
             for(int j=0; j<n; j++)
             {
                 if(!visited[i][j] && grid[i][j] == 1) {
-                    count = 0;
-                    dfs(grid,visited,m,n,i,j);
+                    maxArea = max(maxArea,dfs(grid,visited,m,n,i,j));
                 }
             }
         }
