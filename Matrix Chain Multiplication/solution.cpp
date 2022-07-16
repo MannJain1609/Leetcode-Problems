c2760905a9644cefbbe3da9a62234738b57c9224
Mann Jain <75989468+MannJain1609@users.noreply.github.com>
2022-07-16 20:28:36 +0530

    Update solution.cpp

--- Matrix Chain Multiplication/solution.cpp
+++ Matrix Chain Multiplication/solution.cpp
@@ -9,64 +9,28 @@ using namespace std;
 
 class Solution{
 private:
-    //eval represents what we need the expression to
-    int solve(int n, string s, int start, int end, bool eval, vector<vector<vector<int>>>& dp)
+    int solve(int N, int arr[], int start, int end,vector<vector<int>>& dp)
     {
-        if(start>end)
+        if(start>=end)
             return 0;
-            
-        if(dp[start][end][eval?1:0]!=-1)
-            return dp[start][end][eval?1:0];
-            
-        if (start==end) {
-            if (eval) // Evaluate expression to true
-                return s[start]=='T' ? 1:0;
-            else // Evaluate expression to false
-                return s[start]=='F' ? 1:0;
-        }
-        
-        long long count = 0;
-        for(int k=start+1; k<=end-1; k+=2)
+        if(dp[start][end]!=-1)
+            return dp[start][end];
+        int min_ops = INT_MAX;
+        for(int k=start; k<end; k++)
         {
-            int leftTrue = solve(n,s,start,k-1,true,dp);
-            int leftFalse = solve(n,s,start,k-1,false,dp);
-            int rightTrue = solve(n,s,k+1,end,true,dp);
-            int rightFalse = solve(n,s,k+1,end,false,dp);
-            
-            char opr = s[k];
-           
-            if(opr == '&')
-            {
-                if(eval)
-                    count = count + leftTrue*rightTrue;
-                else
-                    count = count + leftTrue*rightFalse +
-                            leftFalse*rightTrue + leftFalse*rightFalse;
-            }
-            else if(opr == '|')
-            {
-                if(eval)
-                    count = count + leftTrue*rightFalse +
-                            leftFalse*rightTrue + leftTrue*rightTrue;
-                else
-                    count = count + leftFalse*rightFalse;
-            }
-            else
-            {
-                if(eval)
-                    count = count + leftFalse*rightTrue + leftTrue*rightFalse;
-                else
-                    count = count + leftTrue*rightTrue + leftFalse*rightFalse;
-            }
+            int a = solve(N,arr,start,k,dp);
+            int b = solve(N,arr,k+1,end,dp);
+            int temp = a + b + arr[start-1]*arr[k]*arr[end];
+            min_ops = min(min_ops,temp);
         }
-        return dp[start][end][eval?1:0] = (int)count%1003;
+        return dp[start][end] = min_ops;
     }
 public:
-    int countWays(int N, string S){
+    int matrixMultiplication(int N, int arr[])
+    {
         // code here
-        int size = S.size();
-        vector<vector<vector<int>>>dp(N,vector<vector<int>>(N,vector<int>(2,-1)));
-        return solve(N,S,0,size-1,true,dp);
+        vector<vector<int>>dp(N,vector<int>(N,-1));
+        return solve(N,arr,1,N-1,dp);
     }
 };
 
@@ -78,11 +42,12 @@ int main(){
     while(t--){
         int N;
         cin>>N;
-        string S;
-        cin>>S;
+        int arr[N];
+        for(int i = 0;i < N;i++)
+            cin>>arr[i];
         
         Solution ob;
-        cout<<ob.countWays(N, S)<<"\n";
+        cout<<ob.matrixMultiplication(N, arr)<<endl;
     }
     return 0;
 }
