f524a663dac82d000f443bee5ee9585845f76508
Mann Jain <75989468+MannJain1609@users.noreply.github.com>
2022-07-22 11:57:22 +0530

    Time: 9 ms (99.34%), Space: 11.1 MB (29.53%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,27 +1,36 @@
 class Solution {
 public:
     string removeDuplicates(string s, int k) {
-        stack<pair<char,int>>st;
-        for(char c : s)
-        {
-            if(!st.empty() && st.top().first == c) {
-                int count = st.top().second;
-                st.pop();
-                if(count+1<k)
-                    st.push({c,count+1});
-            }
-            else
-                st.push({c,1});
+        int i = 0;
+        vector<int> count(s.size());
+        for(int j=0;j<s.size();j++,i++) {
+            s[i] = s[j];
+            count[i] = ((i > 0 && s[i] == s[i-1]) ? count[i-1] + 1 : 1 );
+            if(count[i] == k)
+                i -= k;
         }
-        string ans="";
-        while(!st.empty())
-        {
-            char c = st.top().first;
-            int k = st.top().second;
-            st.pop();
-            string x(k,c);
-            ans = x+ans;
-        }
-        return ans;
+        return s.substr(0,i);
+        // stack<pair<char,int>>st;
+        // for(char c : s)
+        // {
+        //     if(!st.empty() && st.top().first == c) {
+        //         int count = st.top().second;
+        //         st.pop();
+        //         if(count+1<k)
+        //             st.push({c,count+1});
+        //     }
+        //     else
+        //         st.push({c,1});
+        // }
+        // string ans="";
+        // while(!st.empty())
+        // {
+        //     char c = st.top().first;
+        //     int k = st.top().second;
+        //     st.pop();
+        //     string x(k,c);
+        //     ans = x+ans;
+        // }
+        // return ans;
     }
 };
