71de151f68b25f5e2217455d9a571e78c6d0569d
Mann Jain <75989468+MannJain1609@users.noreply.github.com>
2022-07-30 20:29:38 +0530

    Time: 124 ms (98.20%), Space: 57.4 MB (80.67%) - LeetHub

---
+++ 916-word-subsets/916-word-subsets.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        vector<string> ans;
+        int maxB[26] = {0};
+        for(string ss : words2)
+        {
+            int count[26] = {0};
+            for(char c: ss)
+            {
+                count[c-'a']++;
+            }
+            for(int i=0; i<26; i++)
+                maxB[i] = max(maxB[i],count[i]);
+        }
+        
+        for(string word : words1)
+        {
+            int freq[26] = {0};
+            for(char c : word)
+            {
+                freq[c-'a']++;
+            }
+            int flag = 1;
+            for(int i=0; i<26; i++)
+            {
+                if(freq[i] < maxB[i]) {
+                    flag = 0;
+                    break;
+                }
+            }
+            if(flag)
+                ans.push_back(word);
+        }
+        return ans;
+    }
+};
+
