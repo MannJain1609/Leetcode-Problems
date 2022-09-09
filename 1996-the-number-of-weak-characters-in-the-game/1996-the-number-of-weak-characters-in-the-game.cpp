cd94709f6adbe734130049e6e2ecd8804d7c207a
Mann Jain <75989468+MannJain1609@users.noreply.github.com>
2022-09-09 23:53:35 +0530

    Time: 1392 ms (21.92%), Space: 125.1 MB (91.78%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -1,34 +1,56 @@
+// class Solution {
+// public:
+//     static bool cmp(vector<int> &a, vector<int>& b)
+//     {
+//         if(a[0]!=b[0])
+//             return a[0] < b[0];
+//         return a[1] > b[1];
+//     }
+//     int numberOfWeakCharacters(vector<vector<int>>& properties) {
+//         int numWeak = 0;
+//         int size = properties.size();
+//         sort(properties.begin(),properties.end(),cmp);
+//         unordered_map<int,int>mp;
+//         stack<int>s;
+//         for(int i=size-1; i>=0; i--)
+//         {
+//             while(!s.empty() && properties[s.top()][1] <= properties[i][1])
+//                 s.pop();
+//             if(s.empty())
+//                 mp[i] = -1;
+//             else
+//                 mp[i] = s.top();
+//             s.push(i);
+//         }
+//         for(int i=0; i<size; i++)
+//         {
+//             if(mp[i]!=-1)
+//                 numWeak++;
+//         }
+//         return numWeak;
+      
+//     }
+// };
+
 class Solution {
 public:
-    static bool cmp(vector<int> &a, vector<int>& b)
-    {
-        if(a[0]!=b[0])
-            return a[0] < b[0];
-        return a[1] > b[1];
-    }
     int numberOfWeakCharacters(vector<vector<int>>& properties) {
-        int numWeak = 0;
-        int size = properties.size();
-        sort(properties.begin(),properties.end(),cmp);
-        unordered_map<int,int>mp;
-        stack<int>s;
-        for(int i=size-1; i>=0; i--)
-        {
-            while(!s.empty() && properties[s.top()][1] <= properties[i][1])
-                s.pop();
-            if(s.empty())
-                mp[i] = -1;
-            else
-                mp[i] = s.top();
-            s.push(i);
+        // Sort in ascending order of attack, 
+        // If attack is same sort in descending order of defense
+        sort(properties.begin(), properties.end(), 
+             [](const vector<int>& a, vector<int>& b) -> bool { 
+                 return (a[0] == b[0] ? a[1] > b[1] : a[0] < b[0]);});
+             
+        int weakCharacters = 0;
+        int maxDefense = 0;
+        for (int i = (int)properties.size() - 1; i >= 0; i--) {
+            // Compare the current defense with the maximum achieved so far
+            if (properties[i][1] < maxDefense) {
+                weakCharacters++;
+            }
+            maxDefense = max(maxDefense, properties[i][1]);
         }
-        int count = 0;
-        for(int i=0; i<size; i++)
-        {
-            if(mp[i]!=-1)
-                count++;
-        }
-        return count;
-      
+        
+        return weakCharacters;
     }
 };
