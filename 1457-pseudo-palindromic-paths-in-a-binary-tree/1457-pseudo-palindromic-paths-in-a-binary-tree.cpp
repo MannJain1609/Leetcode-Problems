91912db6aff358892d9a78019800fbf2766a8e7e
Mann Jain <75989468+MannJain1609@users.noreply.github.com>
2022-09-14 23:04:49 +0530

    Time: 883 ms (20.98%), Space: 179.5 MB (88.01%) - LeetHub

---
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    
+    int count = 0;
+    bool isPalindrome(int arr[10])
+    {
+        int odds = 0;
+        for(int i=0; i<10; i++)
+        {
+            if(arr[i]%2!=0)
+                odds++;
+            if(odds>1)
+                return false;
+        }
+        return true;
+    }
+    void helper(TreeNode* root, int arr[10]) {
+        if(root == NULL)
+        {
+            return;
+        }
+        if(root->left == NULL && root->right == NULL)
+        {
+            arr[root->val]++;
+            if(isPalindrome(arr))
+                count++;
+            arr[root->val]--;
+            return;
+        }
+        arr[root->val]++;
+        helper(root->left, arr);
+        helper(root->right, arr);
+        arr[root->val]--;
+    }
+public:
+    int pseudoPalindromicPaths (TreeNode* root) {
+        int arr[10] = {0};
+        helper(root, arr);
+        return count;
+    }
+};
