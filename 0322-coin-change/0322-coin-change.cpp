211089341c025bbc1710f8d19bbbf129943117e4
Mann Jain <mann.jain@iitgn.ac.in>
2024-05-08 01:40:38 +0530

    Time: 23 ms (73.8%), Space: 16 MB (80.92%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<int>dp(amount+1,INT_MAX);
+        dp[0] = 0;
+        for(int i=1; i<=amount; i++) {
+            for(int c: coins) {
+                if(i - c >= 0) {
+                    if(dp[i-c] !=INT_MAX)
+                        dp[i] = min(dp[i], dp[i-c] + 1);
+                }
+            }
+        }
+        if(dp[amount] == INT_MAX)
+            return -1;
+        return dp[amount];
+    }
+};
