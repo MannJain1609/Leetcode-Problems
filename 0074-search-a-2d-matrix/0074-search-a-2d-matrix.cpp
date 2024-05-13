87db5e6b310b9c3765eb533e3f153c6d14328d55
Mann Jain <mann.jain@iitgn.ac.in>
2024-05-13 09:59:52 +0530

    Time: 5 ms (23.63%), Space: 12 MB (48.75%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int low = 0, high = m*n - 1;
+        while (low <= high)
+        {
+            int mid = (low + high)/2;
+            int r = (mid/n);
+            int c = (mid%n);
+            if(matrix[r][c] == target)
+                return true;
+            if(matrix[r][c] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return false;
+    }
+};
